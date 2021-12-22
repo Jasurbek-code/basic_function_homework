2b3b3a1099b457ceee3f8be98233f5f3d4c09714
Jasurbek-code <samyboyborada@gmail.com>
2021-12-23 01:28:43 +0500

    basic function homework

--- basic_func02.py
+++ basic_func02.py
@@ -2,5 +2,5 @@
 # Return the value "Hello World"
 
 def main():                 # funksiya
-    print("Hello World")    # ekranga chiqadigan ma'lumot
+    return print("Hello World")    # ekranga chiqadigan ma'lumot
 main()
